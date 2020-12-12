775c4d50e2d049885b11bdb39d70a0e3d413ecbc
Nicolas SEYS <seys.nicolas@gmail.com>
2020-12-12 19:04:56 +0100

    Add python code template

---
+++ template.py
@@ -0,0 +1,14 @@
+def part1(inp):
+    return 0
+
+
+def part2(inp):
+    return 0
+
+
+if __name__ == "__main__":
+    inp = []
+    with open("xx/input.txt") as f:
+        inp = f.readlines()
+    print(part1(inp))
+    print(part2(inp))
